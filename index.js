503de7f8853f6bdf48ce3b4f9a525cc5d19ef9c5
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-03 05:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
-function happyHolidaysTo(string) {
-  return "Happy holidays, ${string}!"
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${string}!'
 }
